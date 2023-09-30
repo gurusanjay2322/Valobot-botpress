15478191612ed7ccd19893b79fbb0e05bcb2c5a9
Guru Sanjay R K <gurucoolmsd@gmail.com>
2023-09-30 23:46:50 +0530

    Added Loading screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../index.css';
+
+export const Loading = () => {
+  return (
+    <div className='loading-fallback'>
+        <h1>Loading..</h1>
+    </div>
+  )
+}
